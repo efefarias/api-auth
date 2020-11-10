f90bd31006262b48be07ba633a40fbc20400079e
Felipe Alexandre Soares de Farias <efefarias@gmail.com>
2020-11-09 21:25:04 -0300

    Ajustes em dependencias Ajustes nos httpmethods

--- server.js
+++ server.js
@@ -1,49 +1,26 @@
+const bcryptjs = require('bcryptjs')
 const express = require('express')
 const app = express()
-const bcrypt = require('bcryptjs')
-const dbmock = require('./db.json');
+const dbmock = require('./db.json')
 
 app.use(express.json())
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Expose-Headers", "X-Total-Count");
-  res.header("Access-Control-Allow-Methods", "*");
-  next();
-});
-
-//HTTP METHODS OF USERS
-app.get('/users', async (req, res) => {
-  const total = dbmock.users.length;
-  res.set("X-Total-Count", total);
-  res.json(dbmock.users);
-})
-
-app.post('/users', async (req, res) => {
-  try {
-    var salt = bcrypt.genSaltSync(10);
-    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
-    const user = req.body
-    user.password = hashedPassword
-    console.log(req.body)
-    dbmock.users.push(user)
-    res.status(201).send()
-  } catch {
-    res.status(500).send(res.statusText)
-  }
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  res.header("Access-Control-Expose-Headers", "X-Total-Count")
+  res.header("Access-Control-Allow-Methods", "*")
+  next()
 })
 
 app.post('/users/login', async (req, res) => {
+  console.log(req.body)
   const user = dbmock.users.find(user => user.username === req.body.username)
   if (user == null) {
     return res.status(400).send()
   }
   try {
-    if(await bcrypt.compareSync(user.password, req.body.hashedPassword)) {
-      var salt = bcrypt.genSaltSync(10);
-      const hashedPassword = bcrypt.hashSync(user.password, salt);
-      user.password = hashedPassword;
+    if(await bcryptjs.compareSync(user.password, req.body.hashedPassword)) {
       res.json(user)
     } else {
         res.status(401).send() 
@@ -53,62 +30,104 @@ app.post('/users/login', async (req, res) => {
   }
 })
 
-//HTTP METHODS OF POSTS
-app.get('/posts/:id', (req, res) => {
-  const idPost = req.params.id;
-  if(idPost === ''){
-    res.status(500).send(res.statusText)
-  } else {
-    const post = dbmock.posts.find(post => post.id.toString() === idPost);
-    res.json(post);
-  }
+app.get('/users', (req, res) => {
+  const page = req.query.page
+  const limit = req.query.perPage
+  const filter = req.query.filter
+  const order = req.query.order
+
+  const startIndex = (page - 1) * limit
+  const endIndex = page * limit
+
+  const resultUsers = dbmock.users.slice(startIndex, endIndex)
+  
+  const total = dbmock.users.length
+  res.set("X-Total-Count", total)
+  res.json(resultUsers)
 })
 
 app.get('/posts', (req, res) => {
-  const total = dbmock.posts.length;
-  res.set("X-Total-Count", total);
-  res.json(dbmock.posts);
+  const page = req.query.page
+  const limit = req.query.perPage
+  const filter = req.query.filter
+  const order = req.query.order
+
+  const startIndex = (page - 1) * limit
+  const endIndex = page * limit
+
+  const resultPosts = dbmock.posts.slice(startIndex, endIndex)
+  
+  const total = dbmock.posts.length
+  res.set("X-Total-Count", total)
+  res.json(resultPosts)
 })
 
-app.post('/posts', (req, res) => {
-  res.set("Access-Control-Allow-Methods", "POST");
-  console.log(req.body)
-  try {
-    const post = req.body
-    dbmock.posts.push(post)
-    res.status(201).send()
-  } catch {
-    res.status(500).send(res.statusText)
-  }
-})
+//HTTP METHODS OF COMMENTS
+app.get('/comments', (req, res) => {
+  const page = req.query.page
+  const limit = req.query.perPage
+  const filter = req.query.filter
+  const order = req.query.order
 
+  const startIndex = (page - 1) * limit
+  const endIndex = page * limit
 
-//HTTP METHODS OF ROLES
-app.get('/roles', (req, res) => {
-  const total = dbmock.roles.length;
-  res.set("X-Total-Count", total);
-  res.json(dbmock.roles)
-})
-
-//HTTP METHODS OF COMMENT
-app.get('/comments', async (req, res) => {
-  const total = dbmock.comments.length;
-  res.set("X-Total-Count", total);
-  res.json(dbmock.comments);
+  const resultComments = dbmock.comments.slice(startIndex, endIndex)
+  
+  const total = dbmock.comments.length
+  res.set("X-Total-Count", total)
+  res.json(resultComments)
 })
 
 //HTTP METHODS OF ALBUMS
-app.get('/albums', async (req, res) => {
-  const total = dbmock.albums.length;
-  res.set("X-Total-Count", total);
-  res.json(dbmock.albums);
+app.get('/albums', (req, res) => {
+  const page = req.query.page
+  const limit = req.query.perPage
+  const filter = req.query.filter
+  const order = req.query.order
+
+  const startIndex = (page - 1) * limit
+  const endIndex = page * limit
+
+  const resultAlbums = dbmock.albums.slice(startIndex, endIndex)
+  
+  const total = dbmock.albums.length
+  res.set("X-Total-Count", total)
+  res.json(resultAlbums)
 })
 
 //HTTP METHODS OF PHOTOS
-app.get('/photos', async (req, res) => {
-  const total = dbmock.photos.length;
-  res.set("X-Total-Count", total);
-  res.json(dbmock.photos);
+app.get('/photos', (req, res) => {
+  const page = req.query.page
+  const limit = req.query.perPage
+  const filter = req.query.filter
+  const order = req.query.order
+
+  const startIndex = (page - 1) * limit
+  const endIndex = page * limit
+
+  const resultPhotos = dbmock.photos.slice(startIndex, endIndex)
+  
+  const total = dbmock.photos.length
+  res.set("X-Total-Count", total)
+  res.json(resultPhotos)
+})
+
+//HTTP METHODS OF TODOS
+app.get('/todos', (req, res) => {
+  const page = req.query.page
+  const limit = req.query.perPage
+  const filter = req.query.filter
+  const order = req.query.order
+
+  const startIndex = (page - 1) * limit
+  const endIndex = page * limit
+
+  const resultTodos = dbmock.todos.slice(startIndex, endIndex)
+  
+  const total = dbmock.todos.length
+  res.set("X-Total-Count", total)
+  res.json(resultTodos)
 })
 
 app.listen(3001)
